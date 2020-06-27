b64fa41adfc6c0dfce936d193f2056e32666a7fe
kimnana45 <kimle.lek@gmail.com>
2020-06-27 10:42:53 -0400

    testing travis cl

--- models/index.js
+++ models/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
-var fs        = require('fs');
+var fs        = require('fs')
 var path      = require('path');
-var Sequelize = require('sequelize');
+var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
+if (config.use_env_variable)    
+{
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
@@ -29,7 +30,7 @@ Object.keys(db).forEach(function(modelName) {
 
 
 
-            
+
     db[modelName].associate(db);
   }
 });
