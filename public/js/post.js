3020441ed29684ca74ec1d8fb2531f96830b6b71
kimnana45 <kimle.lek@gmail.com>
2020-07-06 13:48:43 -0400

    fixing bugs

--- public/js/post.js
+++ public/js/post.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     $('select').formSelect();
   });
+
+//need to write code for capture user inputs in postItem html. 
+//figure out how to upload picture on na click of a button into the correct div. 
