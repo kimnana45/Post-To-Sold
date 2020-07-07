957c6700ca0b1ce7b2a967661fea752cd63ad6aa
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-07-07 18:32:01 -0400

    updated u in user at model garage_sale line 43

--- models/garage_sale.js
+++ models/garage_sale.js
@@ -40,7 +40,7 @@ module.exports = function(sequelize, DataTypes) {
   garage_sale.associate = function(models) {
     // We're saying that a Post should belong to an Author
     // A Post can't be created without an Author due to the foreign key constraint
-    garage_sale.belongsTo(models.user, {
+    garage_sale.belongsTo(models.User, {
       foreignKey: {
         allowNull: false
       }
