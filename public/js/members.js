4b111505708bd555599a91b21d81ebc3fa2b2ee6
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-07-17 16:19:48 -0400

    fixed the functionality for buttons/updated readme

--- public/js/members.js
+++ public/js/members.js
@@ -1,29 +1,23 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
 });
 
-//jQuery for navbar mobile response
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
-
-
-  // $("#delete").click(function() {
-  //   console.log($(this).attr("data-id"));
-  //   $.ajax({
-  //     method: "DELETE",
-  //     url: "/api/posts/" + $(this).attr("data-id")
-  //   })
-  //     // On success, run the following code
-  //     .then(function() {
-  //       console.log("Deleted Successfully!");
-  //       location.reload()
-  //     });
+$(".delete-btn").click(function() {
+    console.log($(this).attr("data-id"));
+    $.ajax({
+      method: "DELETE",
+      url: "/api/posts/" + $(this).attr("data-id")
+    })
+      .then(function() {
+        console.log("Deleted Successfully!");
+        location.reload()
+      });
     
-  // });
+  });
   
